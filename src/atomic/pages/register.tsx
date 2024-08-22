ed2b6dd38d6c1386f6d34fce8bf5ac4cd367b8e4
DaiNgo <daingo7211@gmail.com>
2024-08-22 19:57:51 +0700

    formik

--- src/atomic/pages/register.tsx
+++ src/atomic/pages/register.tsx
@@ -4,62 +4,73 @@ import * as Y from "yup";
 import { InputWithLabel } from "../components/input";
 import { RadioGroup } from "../components/radio";
 import { signUp } from "../../services/user.service";
-import classNames from 'classnames';
+import classNames from "classnames";
 import { useNavigate } from "react-router-dom";
 
 const schema = Y.object({
-    name: Y.string()
-        .matches(/^[A-Z a-z]+$/, "2")
-        .required("1"),
+  name: Y.string()
+    .matches(/^[A-Z a-z]+$/, "2")
+    .required("1"),
 
-    email: Y.string().email("2").required("1"),
+  email: Y.string().email("2").required("1"),
 
-    password: Y.string()
+  password: Y.string()
 
-        .required("1")
-        .min(8, '2')
-        .matches(/[a-zA-Z]/, '3')
-        .matches(/\d/, '4')
-        .matches(/[!@#$%^&*(),.?":{}|<>]/, '5'),
+    .required("1")
+    .min(8, "2")
+    .matches(/[A-Z]/, "3")
+    .matches(/\d/, "4")
+    .matches(/[!@#$%^&*(),.?":{}|<>]/, "5"),
 
-    confirmPassword: Y.string()
-        .required("1")
-        // @ts-ignore
-        .oneOf([Y.ref('password'), null], '2'),
-
+  confirmPassword: Y.string()
+    .required("1")
+    // @ts-ignore
+    .oneOf([Y.ref("password"), null], "2"),
 });
 
-function ListError({ errors }: {
-    errors:
-    ({ value: string, key: string | number, type: "error" | 'idle' | "success" })[]
+function ListError({
+  errors,
+}: {
+  errors: {
+    value: string;
+    key: string | number;
+    type: "error" | "idle" | "success";
+  }[];
 }) {
-    return <>
-        <ul>
-
-            {errors.map(i => {
-                return <li className={
-                    classNames({
-                        'text-red-500': i.type === 'error',
-                        'text-lime-500': i.type === 'success',
-                        'text-gray-400': i.type === 'idle',
-                    })
-                } key={i.key}>{i.value}</li>
-            })}
-        </ul>
+  return (
+    <>
+      <ul>
+        {errors.map((i) => {
+          return (
+            <li
+              className={classNames({
+                "text-red-500": i.type === "error",
+                "text-lime-500": i.type === "success",
+                "text-gray-400": i.type === "idle",
+              })}
+              key={i.key}
+            >
+              {i.value}
+            </li>
+          );
+        })}
+      </ul>
     </>
+  );
 }
 
 function IIFE(cb: Function) {
-    return cb();
+  return cb();
 }
 
-type TKey = "email" | "name" | 'password' | 'confirmPassword';
+type TKey = "email" | "name" | "password" | "confirmPassword";
 
 type SchemaRequire = {
-    value: string,
-    key: string | number,
-    errors: string[]
-}
+  value: string;
+  key: string | number;
+  errors?: string[];
+  valid?: boolean;
+};
 
 type TSchemaListRequire = Record<TKey, SchemaRequire[]>;
 
@@ -69,176 +80,294 @@ type TSchemaListRequire = Record<TKey, SchemaRequire[]>;
 //     navigate(to)
 // }
 
+// typescript: build
+// schema: yup, zod, : runtime
+
+const usernameSchema = Y.string().min(2).required();
+
+const responseSchema = Y.object({
+  username: Y.string().required(),
+  token: Y.string().required(),
+});
+
+// Từ schema có thể lấy được typescript
+type ResponseSchema = Y.InferType<typeof responseSchema>;
+
+// responseSchema.isValid(response);
+
+/**
+ * response: {
+ *  username: 'fasjldkfjasd',
+ *  token: 'fafasdf',
+ * }
+ * response.token
+ *
+ *
+ *
+ * response: {
+ *  username: 'fasjldkfjasd',
+ *  accessToken: 'fafasdf',
+ * }
+ *
+ * response.token
+ */
+
 export default function Register() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const formik = useFormik({
-        initialValues: {
-            email: "",
-            name: "",
-            password: "",
-            phone: "",
-            gender: '',
-            confirmPassword: '',
-        },
+  // ----------------
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      name: "",
+      password: "",
+      phone: "",
+      gender: "",
+      confirmPassword: "",
+    },
 
-        // validationSchema: schema,
+    onSubmit: (values) => {
+      // Validate
+      if (!isFormValid(schemaListRequire)) return;
 
-        validate: (values) => {
-            schema.validate(values);
-            // const errors = schema.validateSync(values)
+      console.log(values);
+      const { confirmPassword, ...other } = values;
+      const data = {
+        ...other,
+        gender: other.gender === "male" ? true : false,
+      };
 
-            // console.log(errors);
+      signUp(data)
+        .then((resp) => {
+          formik.resetForm();
 
-            return {}
-        },
-        onSubmit: (values) => {
-            console.log(values);
-            const { confirmPassword, ...other } = values;
-            const data = {
-                ...other,
-                gender: other.gender === 'male' ? true : false
-            }
+          navigate("/login");
+        })
+        .catch((e) => {
+          console.log(e);
+          // call api log lỗi
+        });
+    },
+  });
 
-            signUp(data).then(resp => {
-                formik.resetForm();
+  const isFormValid = (schema: TSchemaListRequire) => {
+    return Object.values(schema).every(isFieldValid);
+  };
 
-                navigate('/login');
-            }).catch((e) => {
-                console.log(e);
-                // call api log lỗi
-            })
-        },
-    });
+  const isFieldValid = (field: SchemaRequire[]) => {
+    return field.every((i) => i.valid);
+  };
 
-    // type TK = keyof (typeof formik.touched);
+  const schemaListRequire: TSchemaListRequire = {
+    name: [
+      {
+        value: "Bắt buộc",
+        key: 1,
+        valid: Y.string().required().isValidSync(formik.values.name),
+      },
+      {
+        value: "Không được nhập số",
+        key: 2,
+        valid: Y.string()
+          .matches(/^[A-Z a-z]+$/)
+          .isValidSync(formik.values.name),
+      },
+    ],
+    email: [
+      {
+        value: "Bắt buộc",
+        key: 1,
+        valid: Y.string().required().isValidSync(formik.values.email),
+      },
+      {
+        value: "Không đúng format của email",
+        key: 2,
+        valid: Y.string().email().required().isValidSync(formik.values.email),
+      },
+    ],
+    password: [
+      {
+        value: "Bắt buộc",
+        key: 1,
+        valid: Y.string().required().isValidSync(formik.values.password),
+      },
+      {
+        value: "Ít nhất 8 ký tự",
+        key: 2,
+        valid: Y.string().min(8).required().isValidSync(formik.values.password),
+      },
+      {
+        value: "Có 1 ký tự in hoa",
+        key: 3,
+        valid: Y.string()
+          .matches(/[A-Z]/)
+          .required()
+          .isValidSync(formik.values.password),
+      },
+      {
+        value: "Có 1 ký tự số",
+        key: 4,
+        valid: Y.string()
+          .matches(/\d/)
+          .required()
+          .isValidSync(formik.values.password),
+      },
+      {
+        value: 'Có 1 ký đặc biệt !@#$%^&*(),.?":{}|<>',
+        key: 5,
+        valid: Y.string()
+          .matches(/[!@#$%^&*(),.?":{}|<>]/, "5")
+          .required()
+          .isValidSync(formik.values.password),
+      },
+    ],
+    confirmPassword: [
+      {
+        value: "Bắt buộc",
+        key: 1,
+        valid: Y.string().required().isValidSync(formik.values.confirmPassword),
+      },
+      {
+        value: "Khớp password",
+        key: 2,
+        valid:
+          Y.string().required().isValidSync(formik.values.password) &&
+          formik.values.confirmPassword === formik.values.password,
+      },
+    ],
+  };
 
-    const schemaListRequire: TSchemaListRequire = {
-        name: [
-            { value: "Không được nhập số", key: 2, errors: ["1", "2"] },
-            { value: "Bắt buộc", key: 1, errors: ["1"] },
-        ],
-        email: [
-            { value: "Bắt buộc", key: 1, errors: ["1"] },
-            { value: "Không đúng format của email", key: 2, errors: ["1", "2"] }
-        ],
-        password: [
-            { value: "Bắt buộc", key: 1, errors: ["1"] },
-            { value: "Ít nhất 8 ký tự", key: 2, errors: ["1", "2"] },
-            { value: "Có 1 ký tự in hoa", key: 3, errors: ["1", "3"] },
-            { value: "Có 1 ký tự số", key: 4, errors: ["1", "4"] },
-            { value: "Có 1 ký đặc biệt !@#$%^&*(),.?\":{}|<>", key: 5, errors: ["1", "5"] },
-        ],
-        confirmPassword: [
-            { value: "Bắt buộc", key: 1, errors: ["1"] },
-            { value: "Chưa khớp với password", key: 2, errors: ["1", "2"] },
-        ]
-    }
+  return (
+    <form onSubmit={formik.handleSubmit} className="max-w-[110rem] mx-auto">
+      <Row className="mt-4" gutter={10}>
+        <Col xs={24} sm={12}>
+          <InputWithLabel
+            {...formik.getFieldProps("name")}
+            label={"Name"}
+            placeholder="exmaple: Nguyễn Văn A"
+          />
 
-    const validateType = (key: TKey) => {
-        return (item: SchemaRequire) => {
-            return {
-                ...item,
-                // item.errors.includes(formik.errors.name || ''): kiểm tra có chứa lỗi hay không
-                // !: phủ định
-                // valid: !item.errors.includes(formik.errors.name || ''),
+          {/* <ListError
+            errors={schemaListRequire["name"].map(validateType("name"))}
+          /> */}
 
-                // type: formik.touched.name ?
-                //     item.errors.includes(formik.errors.name || '') ?
-                //         'error' : 'success'
-                //     : 'idle', // 'success', 'idle'
+          <ul>
+            {schemaListRequire.name.map((i) => {
+              return (
+                <li
+                  key={i.key}
+                  style={{
+                    color: i.valid ? "green" : "gray",
+                  }}
+                >
+                  {i.value}
+                </li>
+              );
+            })}
+          </ul>
+        </Col>
 
+        <Col xs={24} sm={12}>
+          <InputWithLabel
+            {...formik.getFieldProps("email")}
+            label={"Email:"}
+            placeholder="example: example@gmail.com"
+          />
 
-                type: IIFE(() => {
-                    // formik.touched.name: true// đã từng chạm vào input
-                    // false: chưa chạm
-                    if (formik.touched[key]) {
-                        // item.errors.includes(formik.errors['key'] || ''): true, có lỗi
-                        // false: không có lỗi, thỏa mãn hết rồi.
-                        if (item.errors.includes(formik.errors[key] || '')) {
-                            return 'error'
-                        }
+          {/* <ListError
+            errors={schemaListRequire["email"].map(validateType("email"))}
+          /> */}
 
-                        return 'success'
-                    }
+          <ul>
+            {schemaListRequire.email.map((i) => {
+              return (
+                <li
+                  key={i.key}
+                  style={{
+                    color: i.valid ? "green" : "gray",
+                  }}
+                >
+                  {i.value}
+                </li>
+              );
+            })}
+          </ul>
+        </Col>
+      </Row>
 
-                    return 'idle'
-                })
-            }
-        }
-    }
+      <Row className="mt-4" gutter={10}>
+        <Col xs={24} sm={12}>
+          <InputWithLabel
+            {...formik.getFieldProps("password")}
+            label={"Password"}
+            placeholder=""
+          />
+          <ul>
+            {schemaListRequire.password.map((i) => {
+              return (
+                <li
+                  key={i.key}
+                  style={{
+                    color: i.valid ? "green" : "gray",
+                  }}
+                >
+                  {i.value}
+                </li>
+              );
+            })}
+          </ul>
+        </Col>
 
-    return (
-        <form onSubmit={formik.handleSubmit} className="max-w-[110rem] mx-auto">
-            <Row className="mt-4" gutter={10}>
-                <Col xs={24} sm={12}>
-                    <InputWithLabel
-                        {...formik.getFieldProps("name")}
-                        label={"Name"}
-                        placeholder="exmaple: Nguyễn Văn A"
-                    />
+        <Col xs={24} sm={12}>
+          <InputWithLabel
+            {...formik.getFieldProps("phone")}
+            label={"Phone"}
+            placeholder="example: +84 123456789"
+          />
+        </Col>
+      </Row>
 
-                    <ListError errors={schemaListRequire['name'].map(validateType('name'))} />
+      <Row className="mt-4" gutter={10}>
+        <Col xs={24} sm={12}>
+          <InputWithLabel
+            {...formik.getFieldProps("confirmPassword")}
+            label={"Password confirm"}
+            placeholder=""
+          />
+          <ul>
+            {schemaListRequire.confirmPassword.map((i) => {
+              return (
+                <li
+                  key={i.key}
+                  style={{
+                    color: i.valid ? "green" : "gray",
+                  }}
+                >
+                  {i.value}
+                </li>
+              );
+            })}
+          </ul>
+        </Col>
 
-                </Col>
+        <Col xs={24} sm={12}>
+          <RadioGroup
+            {...formik.getFieldProps("gender")}
+            data={[
+              {
+                label: "Male",
+                unique: "male",
+              },
+              {
+                label: "female",
+                unique: "female",
+              },
+            ]}
+          />
+        </Col>
+      </Row>
 
-                <Col xs={24} sm={12}>
-                    <InputWithLabel
-                        {...formik.getFieldProps("email")}
-                        label={"Email:"}
-                        placeholder="example: example@gmail.com"
-                    />
-
-                    <ListError errors={schemaListRequire['email'].map(validateType('email'))} />
-
-                </Col>
-
-            </Row>
-
-            <Row className="mt-4" gutter={10}>
-                <Col xs={24} sm={12}>
-                    <InputWithLabel
-                        {...formik.getFieldProps("password")}
-                        label={"Password"}
-                        placeholder=""
-                    />
-                    <ListError errors={schemaListRequire['password'].map(validateType('password'))} />
-                </Col>
-
-                <Col xs={24} sm={12}>
-                    <InputWithLabel
-                        {...formik.getFieldProps("phone")}
-                        label={"Phone"}
-                        placeholder="example: +84 123456789"
-                    />
-                </Col>
-            </Row>
-
-            <Row className="mt-4" gutter={10}>
-                <Col xs={24} sm={12}>
-                    <InputWithLabel
-                        {...formik.getFieldProps("confirmPassword")}
-                        label={"Password confirm"}
-                        placeholder=""
-                    />
-                    <ListError errors={schemaListRequire['confirmPassword'].map(validateType('confirmPassword'))} />
-                </Col>
-
-                <Col xs={24} sm={12}>
-                    <RadioGroup {...formik.getFieldProps("gender")} data={[
-                        {
-                            label: 'Male',
-                            unique: 'male'
-                        },
-                        {
-                            label: 'female',
-                            unique: 'female'
-                        }
-                    ]} />
-                </Col>
-            </Row>
-
-            <button type="submit">Submit</button>
-        </form>
-    );
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
